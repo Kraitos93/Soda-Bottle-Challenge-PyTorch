80fbc558c9eed443043834345ea81330fa02e04b
Rune Eggers <milehigh.thedane@gmail.com>
2019-12-20 12:19:35 +0100

    added get accuracy to test script

--- test.py
+++ test.py
@@ -7,8 +7,21 @@ import torchvision.models as models
 from cnn_architecture import CNN
 from dataloader import bottle_test,data_split
 import sys
-from train_SGD import get_accuracy
 
+def get_accuracy(model,dataloader):
+	model.eval()
+	correct = 0
+	total = 0
+	with torch.no_grad():
+		for data in dataloader:
+			images, labels = data
+			images,labels = images.to(device), labels.to(device)
+			outputs = model(images)
+			_, predicted = torch.max(outputs.data, 1)
+			total += labels.size(0)
+			correct += (predicted == labels).sum().item()
+
+	return (100. * correct / total)
 
 def define_loader(dataset_path):
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225])
